aab3e679bcb3aa0377dfa3c90ab56703fc02839b
pbailey212 <pbailey212@gmail.com>
2014-10-08 13:21:13 -0700

    madhid2lte_test
    test build

---
+++ BoardConfig.mk
@@ -0,0 +1,81 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from common msm8960
+-include device/samsung/msm8960-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/d2lte/include
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := d2att,d2spr,d2spi,d2tfnspr,d2tmo,d2cri,d2mtr,d2usc,d2vmu,d2vzw,d2lte,d2can
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+
+# Kernel
+TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
+
+# Audio
+BOARD_HAVE_AUDIENCE_A2220 := true
+BOARD_HAVE_DOCK_USBAUDIO := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
+BOARD_USES_FLUENCE_FOR_VOIP := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_VOIP := true
+
+# Bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2lte/bluetooth/vnd_d2.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2lte/bluetooth
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Camera
+TARGET_ADD_ISO_MODE_1600 := true
+TARGET_NEED_CAMERA_ZSL := true
+TARGET_NEED_EXPOSURE_HACK := true
+TARGET_NEED_SAMSUNG_CAMERA_MODE := true
+
+# GPS
+TARGET_NO_RPC := true
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/d2lte/ril
+
+# Wifi
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HAVE_SAMSUNG_WIFI := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+
+# Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_d2lte
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/d2lte/init/init_d2lte.c
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/d2lte
